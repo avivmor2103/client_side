9230eacce5408fc1e5a3fb890e0bdc81e7c10578
Aviv Mor <avivmore88@gmail.com>
2022-09-02 01:32:12 +0300

    order cards connect to server side + filters and update status

--- src/components/ChefPage/ChefPage.js
+++ src/components/ChefPage/ChefPage.js
@@ -1,109 +1,130 @@
-import React , {useState, useRef} from 'react';
+import React , {useState, useRef , useEffect} from 'react';
+import {default as axios} from 'axios';
 import './ChefPage.css';
 import OrderCard from './OrderCard';
 
-const arr= [
-    {
-        orderNumber: 1,
-        date: `${new Date().toDateString()}`,
-        tableNumber: 3,
-        status: 'served',
-        ordersList: [
-            {
-                name:'Pizza',
-                comments: 'with Fries',
-                amount: 1
-            },
-            {
-                name: 'Chiken',
-                comments: 'with Rice',
-                amount: 1
-            },
-            {
-                name: 'Hamburger',
-                comments: 'with Fries',
-                amount: 1
-            }
-        ]
-    },
-    {
-        orderNumber: 2,
-        date: `${new Date().toDateString()}`,
-        tableNumber: 4,
-        status: 'in preparation',
-        ordersList:[
-            {
-                name:'Pizza',
-                comments: 'with Fries',
-                amount: 1
-            },
-            {
-                name: 'Chiken',
-                comments: 'with Rice',
-                amount: 1
-            }
-        ]
-    },
-    {
-        orderNumber: 3,
-        date: `${new Date().toDateString()}`,
-        tableNumber: 5,
-        status: 'in preparation',
-        ordersList: [
-            {
-                name:'Hamburger',
-                comments: 'with Zuccini',
-                amount: 1
-            },
-            {
-                name: 'Chiken',
-                comments: 'with Salad',
-                amount: 1
-            }
-        ]
-    },
-    {
-        orderNumber: 4,
-        date: `${new Date().toDateString()}`,
-        tableNumber: 7,
-        status: 'in preparation',
-        ordersList:[
-            {
-                name:'Hamburger',
-                comments: 'with Fries',
-                amount: 1
-            },
-            {
-                name: 'Chiken',
-                comments: 'with Rice',
-                amount: 1
-            }
-        ]
-    },
-    {
-        orderNumber: 5,
-        date: `${new Date().toDateString()}`,
-        tableNumber: 5,
-        status: 'in preparation',
-        ordersList:[
-            {
-                name:'Hamburger',
-                comments: 'Kosher',
-                amount: 1
-            },
-            {
-                name: 'Pizza',
-                comments: 'Extra cheese',
-                amount: 1
-            }
-        ]
-    }
-];   
+
+// const arr= [
+//     {
+//         orderNumber: 1,
+//         date: `${new Date().toDateString()}`,
+//         tableNumber: 3,
+//         status: 'served',
+//         ordersList: [
+//             {
+//                 name:'Pizza',
+//                 comments: 'with Fries',
+//                 amount: 1
+//             },
+//             {
+//                 name: 'Chiken',
+//                 comments: 'with Rice',
+//                 amount: 1
+//             },
+//             {
+//                 name: 'Hamburger',
+//                 comments: 'with Fries',
+//                 amount: 1
+//             }
+//         ]
+//     },
+//     {
+//         orderNumber: 2,
+//         date: `${new Date().toDateString()}`,
+//         tableNumber: 4,
+//         status: 'in preparation',
+//         ordersList:[
+//             {
+//                 name:'Pizza',
+//                 comments: 'with Fries',
+//                 amount: 1
+//             },
+//             {
+//                 name: 'Chiken',
+//                 comments: 'with Rice',
+//                 amount: 1
+//             }
+//         ]
+//     },
+//     {
+//         orderNumber: 3,
+//         date: `${new Date().toDateString()}`,
+//         tableNumber: 5,
+//         status: 'in preparation',
+//         ordersList: [
+//             {
+//                 name:'Hamburger',
+//                 comments: 'with Zuccini',
+//                 amount: 1
+//             },
+//             {
+//                 name: 'Chiken',
+//                 comments: 'with Salad',
+//                 amount: 1
+//             }
+//         ]
+//     },
+//     {
+//         orderNumber: 4,
+//         date: `${new Date().toDateString()}`,
+//         tableNumber: 7,
+//         status: 'in preparation',
+//         ordersList:[
+//             {
+//                 name:'Hamburger',
+//                 comments: 'with Fries',
+//                 amount: 1
+//             },
+//             {
+//                 name: 'Chiken',
+//                 comments: 'with Rice',
+//                 amount: 1
+//             }
+//         ]
+//     },
+//     {
+//         orderNumber: 5,
+//         date: `${new Date().toDateString()}`,
+//         tableNumber: 5,
+//         status: 'in preparation',
+//         ordersList:[
+//             {
+//                 name:'Hamburger',
+//                 comments: 'Kosher',
+//                 amount: 1
+//             },
+//             {
+//                 name: 'Pizza',
+//                 comments: 'Extra cheese',
+//                 amount: 1
+//             }
+//         ]
+//     }
+// ];   
 
 const ChefPage = (props) => {
-    const [ ordersArray, setOrdersArray] = useState(arr);
+    const [ ordersArray, setOrdersArray] = useState([]);
+    const [ filteredOrdersArray, setFilteredOrdersArray] = useState([]);
+    const [filter, setFilter] = useState('All Orders');
     const dragItem = useRef();
     const dragOverItem = useRef();
+
+    useEffect(()=>{
+        const url = 'http://localhost:3001/api/order/all_orders';
+        
+        const getAllOrders = async ()=>{
+            try{
+                const response = await axios.get(url);
+                console.log(response);
+                setOrdersArray(response.data);
+                setFilteredOrdersArray(response.data);
+                
+            }catch(e){
+                console.log(e);
+            }
+        }
+        getAllOrders();
+    }, [])
 
     const dragStart = (e, position) => {
         dragItem.current = position;    
@@ -120,22 +141,64 @@ const ChefPage = (props) => {
         dragItem.current = null;
         dragOverItem.current = null;
         setOrdersArray(copyListItems);
+        setFilteredOrdersArray(copyListItems);
     };
+
+    const updateOrdersArrayHandler = (ordersAfterUpdate) => {
+        setOrdersArray(ordersAfterUpdate);
+        setFilteredOrdersArray(ordersAfterUpdate);
+    }
+
+    const onFilterSelectClick = (event) => {
+        const value = (event.target.value);
+        console.log(value);
+       
+        setFilter(value);
+        setOrdersArray(filteredOrdersArray);
+    
+       
+        return;
+    }
    
     return (
         <div>
             <div className='title-container'>
                 Chef Page
             </div>
+            <div className='select-container'>
+                <select className='filter-order-select' onClick={onFilterSelectClick}>
+                    <option value="" disabled>Select</option>   
+                    <option className='filter-option'>All Orders</option>
+                    <option>Pre preparation</option>
+                    <option>In preparation</option>
+                    <option>Post preparation</option>                    
+                </select>
+            </div>
             <div className='orders-container'>
-                { ordersArray.map((item , index)=>{ return (<div className='order-container'
-                                                        key={index} 
-                                                        onDragStart={(e) => dragStart(e, index)} 
-                                                        onDragEnter={(e) => dragEnter(e, index)} 
-                                                        onDragEnd={drop}
-                                                        draggable>
-                                                            <OrderCard orderData={item}/>
-                                                        </div>)})}
+                { ordersArray.map((item , index)=>{ return filter === 'All Orders'?
+                                                        <div className='order-container'
+                                                            key={index} 
+                                                            onDragStart={(e) => dragStart(e, index)} 
+                                                            onDragEnter={(e) => dragEnter(e, index)} 
+                                                            onDragEnd={drop}
+                                                            draggable
+                                                            >
+                                                            <OrderCard orderData={item} updateOrdersArray={updateOrdersArrayHandler}/>
+                                                        </div>
+                                                        :
+                                                        (item.status === filter ? <div className='order-container'
+                                                            key={index} 
+                                                            onDragStart={(e) => dragStart(e, index)} 
+                                                            onDragEnter={(e) => dragEnter(e, index)} 
+                                                            onDragEnd={drop}
+                                                            draggable
+                                                            >
+                                                            <OrderCard orderData={item} updateOrdersArray={updateOrdersArrayHandler}/>
+                                                        </div> 
+                                                        :
+                                                         null )
+                                                    
+                                                })}
             </div>
         </div>
     );
--- src/components/ChefPage/OrderCard.js
+++ src/components/ChefPage/OrderCard.js
@@ -1,41 +1,86 @@
-import React from 'react';
+import axios from 'axios';
+import React , {useState} from 'react';
 import './OrderCard.css';
 
 const OrderCard = (props) => {
+
+    const [orderStatus , setOrderStatus] = useState(props.orderData.status);
+
+    const selectOrderStatus = (event) => {
+        setOrderStatus(event.target.value);
+        const url = 'http://localhost:3001/api/order/update_status';
+        const body = {
+            newStatus : (event.target.value),
+            orderNumber : props.orderData.numOrder
+        }
+
+        const updateOrderStatus = async () => {
+            
+            try{
+                const response = await axios.put(url , body);
+                props.updateOrdersArray(response.data);
+            }catch(e){
+                console.log(e);
+            }
+        }
+        updateOrderStatus();
+    }
+
+    const onDeleteOrderFromArray = () => {
+        const url = 'http://localhost:3001/api/order/delete/' + props.orderData.numOrder;
+
+        const deleteOrder = async () => { 
+            try { 
+                const response = await axios.delete(url);
+                props.updateOrdersArray(response.data);
+            }catch(e){
+                console.log(e);
+            }
+        }
+        deleteOrder();
+    }
+
+    
     return (
-        <div className='order-card-container'>
-            <div className='detailes-container'>
-                <div>Order No. {props.orderData.orderNumber}</div>
-                <div>Date {props.orderData.date}</div>
-                <div>Table No.{props.orderData.tableNumber}</div>
-                <div>Status: {props.orderData.status}</div>
+        <div className= {orderStatus === "Pre preparation"? 'order-card-container' : ( orderStatus === 'In preparation'? 'order-card-container-in' : 'order-card-container-post')}>
+            <div className='top-card-div'>
+                <div className='detailes-container'>
+                    <div>Order No. {props.orderData.numOrder}</div>
+                    <div>Date {props.orderData.date}</div>
+                    <div>Time {props.orderData.time}</div>
+                    <div>Table No.{props.orderData.tableNumber}</div>
+                    <div>Employee: {props.orderData.employeeName}</div>
+                </div>
+                <button onClick={onDeleteOrderFromArray}>X</button>
             </div>
             <div className='items-table-container'>
                 <div className='column-container'>
-                    <div  className='row-container'>Item Name</div>
+                    <div  className='row-container'>Name</div>
                     {
-                        props.orderData.ordersList.map((item, index)=> { return <div key={index}>{item.name}</div>})
+                        props.orderData.itemsList.map((item, index)=> { return <div key={index}>{item.name}</div>})
                     }
                 </div>
                 <div className='column-container'>
-                    <div className='row-container'>Comments</div>
+                    <div className='row-container'>Notes</div>
                     {
-                        props.orderData.ordersList.map((item, index)=> { return <div key={index}>{item.comments}</div>})
+                        props.orderData.itemsList.map((item, index)=> { return <div key={index}>{item.notes}</div>})
                     }
                 </div>
                 <div>
-                    <div className='row-container'>Amount</div>
+                    <div className='row-container'>Quantity</div>
                     {
-                        props.orderData.ordersList.map((item, index)=> { return <div key={index}>{item.amount}</div>})
+                        props.orderData.itemsList.map((item, index)=> { return <div key={index}>{item.quantity}</div>})
                     }
                 </div>
-                {/* <div>
-                    {
-                        props.orderData.ordersList
-                    }
-                </div> */}
                 
             </div>
+            <select onClick={selectOrderStatus} className="select-status">
+                    <option>{orderStatus}</option>
+                    <option>Pre preparation</option>
+                    <option>In preparation</option>
+                    <option>Post preparation</option>
+
+            </select>
         </div>
     );
 }
